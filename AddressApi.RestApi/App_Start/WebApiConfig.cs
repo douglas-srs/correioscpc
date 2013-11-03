ace74e2c3fde9b610d16f6ccab9872f9dfa78328
Douglas Soares <douglas.srs12@gmail.com>
2013-11-03 06:28:42 -0200

    Ok

---
+++ AddressApi.RestApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using WebApiContrib.Formatting.Jsonp;
+
+namespace AddressApi.RestApi.App_Start
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "cep/{zipCode}",
+                defaults: new { controller = "Address", action = "Get" },
+                constraints: new { zipCode = @"^\d+$" }
+            );
+
+            var jsonpFormatter = new JsonpMediaTypeFormatter { Indent = true };
+
+            config.Formatters.Insert(0, jsonpFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.Indent = true;
+            config.EnableSystemDiagnosticsTracing();
+        }
+    }
+}
